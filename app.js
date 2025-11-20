570e292f1ccb2958c6168adedb57b7f159a27fe8
Abhay Bhardwaj <abhaysippy307@gmail.com>
2025-11-20 15:59:44 +0530

    download feature

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
-let btn = document.querySelector("button");
-let main = document.querySelector("main");
-const quotes = [
-  "Keep moving forward.",
-  "Progress starts with action.",
-  "Trust your own pace.",
-  "Discipline creates true freedom.",
-  "Dream big, start small.",
-  "Consistency builds real success.",
-  "Focus on the goal.",
-  "Small steps change everything.",
-  "Believe, act, achieve.",
-  "Create your own path.",
-];
+let btn = document.querySelector(".card .lower button");
+let h2 = document.querySelector(".lower h2");
+let inner = document.querySelector(".inner");
+let progress = 0;
 
 btn.addEventListener("click", function () {
-  let x = Math.random() * 80;
-  let y = Math.random() * 80;
-  let r = Math.random() * 20;
-  let s = Math.random() * 2;
-  let c1 = Math.floor(Math.random() * 256);
-  let c2 = Math.floor(Math.random() * 256);
-  let c3 = Math.floor(Math.random() * 256);
-  let randomQuote = Math.floor(Math.random() * quotes.length);
-  let h1 = document.createElement("h1");
-  h1.innerHTML = quotes[randomQuote];
-  h1.style.position = "absolute";
-  h1.style.top = y + "%";
-  h1.style.left = x + "%";
-  h1.style.rotate = r + "deg";
-  h1.style.scale = s;
-  h1.style.color = `rgb(${c1},${c2},${c3})`;
-  main.appendChild(h1);
+  let time = 30 + Math.floor(Math.random() * 50);
+  console.log(time / 10, "seconds");
+  btn.style.pointerEvents = "none";
+  let stop = setInterval(() => {
+    progress++;
+    h2.innerHTML = progress + "%";
+    inner.style.width = progress + "%";
+  }, time);
+
+  setTimeout(() => {
+    clearInterval(stop);
+    btn.innerHTML = "Downloaded";
+    btn.style.opacity = "0.5";
+  }, time * 100);
 });
