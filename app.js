3625f35ee076aa5019fd288329687368afe29eda
Abhay Bhardwaj <abhaysippy307@gmail.com>
2025-11-20 16:21:08 +0530

    instafeature

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-let btn = document.querySelector(".card .lower button");
-let h2 = document.querySelector(".lower h2");
-let inner = document.querySelector(".inner");
-let progress = 0;
+const img = document.querySelector('.card img')
+const love = document.querySelector('#love')
 
-btn.addEventListener("click", function () {
-  let time = 30 + Math.floor(Math.random() * 50);
-  console.log(time / 10, "seconds");
-  btn.style.pointerEvents = "none";
-  let stop = setInterval(() => {
-    progress++;
-    h2.innerHTML = progress + "%";
-    inner.style.width = progress + "%";
-  }, time);
-
+img.addEventListener('dblclick', function(){
+  love.style.transform = 'translate(-50%, -50%) scale(1)'
+  love.style.opacity = 1
   setTimeout(() => {
-    clearInterval(stop);
-    btn.innerHTML = "Downloaded";
-    btn.style.opacity = "0.5";
-  }, time * 100);
-});
+      love.style.transform = 'translate(-50%, -50%) scale(0)'
+  love.style.opacity = 0  
+  },1000 );
+})
