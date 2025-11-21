9cd9636b572dfe3afb38642bc5f198362341f35e
Abhay Bhardwaj <abhaysippy307@gmail.com>
2025-11-21 22:46:12 +0530

    keypress event

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-let body = document.body
-let cursor = document.querySelector('#cursor')
-
-body.addEventListener('mousemove', function(dets){
-  cursor.style.top = dets.y+"px"
-  cursor.style.left = dets.x+"px"
+document.body.addEventListener('keypress', function(e){
+  document.querySelector('main h1').innerHTML = e.code
 })
